89b814c7deed161d36c5be41cbaf8693c16b9c01
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2022-10-17 00:19:49 +0200

    Add files via upload

---
+++ ponavljanje2.2/ponavljanje2.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanje2._2
+{
+    class Stablo
+    {
+        private bool otpadajuListovi;
+        string vrstaStabla;
+
+        public Stablo(bool otpadajuListovi)
+        {
+            OtpadajuListovi = otpadajuListovi;
+        }
+
+        public bool OtpadajuListovi { get => otpadajuListovi; set => otpadajuListovi = value; }
+        public override string ToString()
+        {
+            if (OtpadajuListovi == true)
+            { vrstaStabla = "bijelogoricno"; }
+            else
+            { vrstaStabla = "crnogoricno"; }
+            return ("Otpadaju listovi: " + OtpadajuListovi + " Vrsta stabla: " + vrstaStabla);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stablo stablo1 = new Stablo(false);
+            Stablo stablo2 = new Stablo(true);
+            Console.WriteLine(stablo1.ToString());
+            Console.WriteLine(stablo2.ToString());
+            Console.ReadKey();
+        }
+    }
+}
